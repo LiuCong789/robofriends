b52f007ec91bb900fd25a570b8b4a193a37764af
LiuCong789 <cliu77@163.com>
2023-04-12 11:49:20 +1000

    Update the heading

--- src/robots.js
+++ src/robots.js
@@ -31,7 +31,7 @@ const robots = [
   },
   {
     id: 6,
-    name: "Mrs. Dennis Schulist",
+    name: "Mrs. Denny Schulit",
     username: "Leopoldo_Corkery",
     email: "Karley_Dach@jasper.info",
   },
